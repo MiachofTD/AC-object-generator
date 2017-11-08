9a6f1d1d87f22bfb58d1c9afa31049737678f3e3
Lisa Riggle <julisana@gmail.com>
2017-11-07 21:35:33 -0600

    Display browser warnings for IE/Edge.
     Changes to be committed:
    	modified:   nav.php

--- nav.php
+++ nav.php
@@ -30,3 +30,17 @@ foreach ( $files as $name => $file ) {
     }
 }
 echo '</h4>';
+
+if (
+    (
+        strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Chrome' ) === false &&
+        strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Firefox' ) === false &&
+        strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Safari' ) === false
+    ) || strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Edge' ) !== false
+) { ?>
+    <div class="row">
+        <div class="alert alert-warning col-xs-10">
+            This application is not guaranteed compatible with your browser. Please switch to Chrome if you have any issues.
+        </div>
+    </div>
+<?php }
